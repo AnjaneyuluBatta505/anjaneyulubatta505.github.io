cef987f1121ce36727fd1c8bdb9e288ce28ef89c
Anjaneyulu Batta <anjaneyulu.batta505@gmail.com>
2024-09-17 02:18:39 +0550

    Deployed 57779787 with MkDocs version: 1.1.2

---
+++ assets/javascripts/adblocker-detector.js
@@ -0,0 +1,27 @@
+async function detectAdBlock() {
+  let adBlockEnabled = false;
+  const googleAdUrl =
+    "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+  try {
+    await fetch(new Request(googleAdUrl)).catch((_) => (adBlockEnabled = true));
+  } catch (e) {
+    adBlockEnabled = true;
+  } finally {
+    console.log(`AdBlock Enabled: ${adBlockEnabled}`);
+    return adBlockEnabled;
+  }
+}
+
+setTimeout(async function () {
+  const isBlocked = await detectAdBlock();
+  const adBlockDiv = document.getElementById("ad-block-message");
+  const closeBtn = document.getElementById("close-message");
+  if (isBlocked) {
+    adBlockDiv.style.display = "block";
+    closeBtn.addEventListener("click", () => {
+      adBlockDiv.remove();
+    });
+  } else {
+    adBlockDiv.style.display = "none";
+  }
+}, 1000);
--- assets/javascripts/google-adsense-adds.js
+++ assets/javascripts/google-adsense-adds.js
@@ -1,27 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var getAddCode = function(name) {
-        var data = "<!-- add -->";
-        switch (name) {
-            case "in-article":
-                data = '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8955684300110547" data-ad-slot="3366278060"></ins>';
-                break
-            case "link":
-                data = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8955684300110547" data-ad-slot="5817528115" data-ad-format="link" data-full-width-responsive="true"></ins>';
-                break
-            case "auto":
-                data = '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8955684300110547" data-ad-slot="7228096482" data-ad-format="auto" data-full-width-responsive="true"></ins>';
-                break
-        }
-        return data
-    };
-    var insertAddUnit = function(name) {
-        return getAddCode(name)
+document.addEventListener("DOMContentLoaded", function (event) {
+  var getAddCode = function (name) {
+    var data = "<!-- add -->";
+    switch (name) {
+      case "article":
+        data =
+          '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-8955684300110547" data-ad-slot="3366278060"></ins>';
+        break;
+      case "link":
+        data =
+          '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8955684300110547" data-ad-slot="5817528115" data-ad-format="link" data-full-width-responsive="true"></ins>';
+        break;
+      case "auto":
+        data =
+          '<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8955684300110547" data-ad-slot="7228096482" data-ad-format="auto" data-full-width-responsive="true"></ins>';
+        break;
     }
-    var inArticeAds = document.querySelectorAll('.add-in-article');
-    inArticeAds.forEach(function(item, index) {
-        var name = "in-article";
-        item.innerHTML = insertAddUnit(name);
-        (adsbygoogle = window.adsbygoogle || []).push({});
+    return data;
+  };
+  var insertAddUnit = function (name) {
+    return getAddCode(name);
+  };
+  const addTypes = ["article", "link", "auto"];
+  addTypes.map((name) => {
+    var adds = document.querySelectorAll(`.add-in-${name}`);
+    adds.forEach(function (item, index) {
+      console.log(">>>loaded add", item, name);
+      item.innerHTML = insertAddUnit(name);
+      (adsbygoogle = window.adsbygoogle || []).push({});
     });
-    console.log("Google Adsense Checking...")
-})
+  });
+  console.log("Google Adsense Checking...");
+});
